25f4160f2c77cdf5f42f4bc7aaf351f1cbb9ab89
John Patrick <johnpatrick3@me.com>
2023-09-13 19:38:39 -0400

    Got WebScraping to work

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
 
 stock_urls = {'apple': 'https://finance.yahoo.com/quote/AAPL/history?period1=1536192000&period2=1693958400&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true',
               'microsoft': 'https://finance.yahoo.com/quote/MSFT/history?period1=1536278400&period2=1694044800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true',
@@ -10,10 +14,81 @@ stock_urls = {'apple': 'https://finance.yahoo.com/quote/AAPL/history?period1=153
               'google': 'https://finance.yahoo.com/quote/GOOG/history?period1=1536278400&period2=1694044800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true',
               'meta': 'https://finance.yahoo.com/quote/META/history?period1=1536278400&period2=1694044800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true',
               'proctor & gamble': 'https://finance.yahoo.com/quote/PG/history?period1=1536278400&period2=1694044800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true',
-              'amazon': 'https://finance.yahoo.com/quote/AMZN/history?period1=1536278400&period2=1694044800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true',
-              
-
-
-
-
+              'amazon': 'https://finance.yahoo.com/quote/AMZN/history?period1=1536278400&period2=1694044800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true'
 }
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+
+viable_browser_options = {1: "Chrome",
+                          2: "Safari",
+                          3: "Firefox"}
+
+while True:
+    user_browser_selection = input("Enter the number corresponding to your browser of choice: (1) Chrome, (2) Safari, (3) Firefox")
+
+    try:
+        user_browser_selection = int(user_browser_selection)
+
+    except ValueError:
+        print("Value entered is not a valid input option")
+    
+    if user_browser_selection not in viable_browser_options.keys():
+        raise Exception("User did not enter the number corresponding to one of the viable browser options (1, 2, or 3) ")
+        continue
+    else:
+        break
+
+
+match user_browser_selection:
+    case 1:
+        browser = webdriver.Chrome()
+    case 2:
+        browser = webdriver.Safari()
+    case 3:
+        browser = webdriver.Firefox()
+
+browser.get(stock_urls['apple'])
+i = 0
+while i < 50:
+    browser.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+    time.sleep(1)
+    i += 1
+
+#with open('saved_webpage.html',"w", encoding = 'utf-8') as f:
+#    f.write(browser.page_source)
+
+time.sleep(10)
+#browser.close()
+#//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr[1]/td[3]
+elements = browser.find_elements(By.XPATH, "//*[@id='Col1-1-HistoricalDataTable-Proxy']/section/div[2]/table")
+for elementValue in elements:
+    print(elementValue.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for i in stock_urls.keys():
+#html_text = (requests.get(stock_urls['apple'],headers=headers)).text
+saved_values = []
+
+#with open("C:\\saved_webpage.html") as fp:
+#    soup = BeautifulSoup(fp, 'html.parser')
+#    stock_open = soup.find()
+#    stock_close = 
+#    stock_date = 
+#    company = 
+#    daily_stock_values = [company, stock_date, stock_open, stock_close]
+
+#    saved_values.append(daily_stock_values)
+
+
